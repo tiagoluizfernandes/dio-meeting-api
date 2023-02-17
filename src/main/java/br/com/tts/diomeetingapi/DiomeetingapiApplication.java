1a1c51e570299756973f3ac9b018e5a205b993ce
Tiago Luiz Fernandes <tiagoluiz@gft.com>
2023-02-17 09:30:12 -0300

    Review

---
+++ src/main/java/br/com/tts/diomeetingapi/DiomeetingapiApplication.java
@@ -0,0 +1,11 @@
+package br.com.tts.diomeetingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiomeetingapiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DiomeetingapiApplication.class, args);
+	}
+}
